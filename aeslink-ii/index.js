c055d1b9c8dd16b68792fe61094839b6a7feed95
cyraki <34148795+cykifish@users.noreply.github.com>
2023-02-25 11:12:25 -0500

    Commit 001

--- aeslink-ii/index.js
+++ aeslink-ii/index.js
@@ -1,14 +1,18 @@
 // Aeslink-ii
 // by Cyra
 
+// Starts AESLINK-II //
 console.log('Initialize...');
-$('#edit_menu').css('right','-336px')
 
 $('#edit_button').on('click', ()=>{
 	let right = $('#edit_menu').css('right');
 	if (right == '-336px') {
 		$('#edit_menu').css('right','16px');
+		$('#edit_button').attr('open',true);
+
 	} else if (right == '16px') {
 		$('#edit_menu').css('right','-336px');
+		$('#edit_button').attr('open',false);
 	}
+
 });
